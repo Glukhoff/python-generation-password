e26c30ee0ff5cacdc263ff4f8f5e84259e2c965a
glukhovsergey <ahref5@yandex.ru>
2020-03-19 13:00:28 +0300

    Логика программы переработана

--- pgp.py
+++ pgp.py
@@ -22,7 +22,7 @@ Version: 0.4 | 2020
 def num_char():
     while True:
         try:
-            number_of_characters = int(input('Количество символов: '))
+            number_of_characters = int(input('Для генерации пароля укажите необходимое' + '\n' + 'количество символов: '))
             break
         except:
             print('На ввод принимаются только целые числа, попробуйте еще раз.')
@@ -33,7 +33,7 @@ def num_char():
 def copy_clip(copy_object):
     try:
         pyperclip.copy(copy_object)
-        print('Пароль успешно скопирован в буфер обмена')
+        print('Пароль скопирован в буфер обмена')
     except:
         print('Ошибка при копировании пароля в буфер обмена')
 
@@ -42,24 +42,30 @@ def randpass():
     result = ''
     for _ in range(num_char()):
         result += alphabet[random.randint(0, 74)]
-    print('Пароль: ' + result)
+    print('''
+--------------------------------------
+Пароль: ''' + result + '''
+--------------------------------------''')
     copy_clip(result)
     return result
 
 
 def save_pass(randpass=randpass()):
-    save = str(input('Сохранить его в файл? [y/N]: '))
-    if save in yes:
-        input_name_file = input('Имя файла: ')
-        login = input('Логин: ')
-        f = open(os.getenv('HOME') + '/Desktop/' + input_name_file + '.txt', 'w')
-        f.write(author + '\n\n' + 'Логин: ' + login + '\n' + 'Пароль: ' + randpass)
-        f.close()
-        print("Пароль сохранен.")
-    elif save in no:
-        print('Пароль не был сохранён, но мы не рекомендуем так делать!')
-    else:
-        print('\n' + 'Введено не верное значение' + '\n')
-
+    while True:
+        save = str(input('Сохранить его в файл? [y/N]: '))
+        if save in yes:
+            input_name_file = input('Имя файла: ')
+            login = input('Укажите логин которому принадлежит этот пароль: ')
+            f = open(os.getenv('HOME') + '/Desktop/' + input_name_file + '.txt', 'w')
+            f.write(author + '\n\n' + 'Логин: ' + login + '\n' + 'Пароль: ' + randpass)
+            f.close()
+            print('Готово')
+            break
+        elif save in no:
+            print('Пароль не был сохранён, но мы не рекомендуем так делать!')
+            break
+        else:
+            print('\n' + 'Введено не верное значение' + '\n')
+            continue
 
 save_pass()
