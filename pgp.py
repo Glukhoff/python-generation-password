1c944e1d30cbd9644677a3ce9b69db6ca0f59ba6
glukhoff <ahref5@yandex.ru>
2020-07-05 23:25:56 +0300

    update program

--- pgp.py
+++ pgp.py
@@ -1,6 +1,7 @@
-import random
 import pyperclip
-import os, string
+import random
+import string
+import os
 
 author = 'Пароль сгенерирован с помошью ~ Python Generator Password ~'
 yes = ['Y', 'y', 'Д', 'д']
@@ -8,24 +9,23 @@ yes = ['Y', 'y', 'Д', 'д']
 print('''
 --------------------------------------
 Python Generator Password - for Mac OS
-Version: 1.2 | 2020 
+Version: 1.3 | 2020 
 --------------------------------------
 ''')
 
 
-def format_password(password):
-    return "-" * 30 + "\nПароль: {}\n".format(password) + "-" * 30
+def format_password(result):
+    return "-" * 30 + "\nПароль: {}\n".format(result) + "-" * 30
 
 
 def num_char():
     while True:
         try:
-            number_of_characters = int(input('Для генерации пароля укажите необходимое\nколичество символов: '))
-            break
-        except:
+            get_number = int(input('Для генерации пароля укажите необходимое\nколичество символов: '))
+            return get_number
+        except TypeError:
             print('На ввод принимаются только целые числа, попробуйте еще раз.')
             continue
-    return number_of_characters
 
 
 def copy_clip(copy_object):
@@ -38,23 +38,23 @@ def _random_pass():
     result = ''
     for _ in range(num_char()):
         result += random.choice(alphabet)
-    print(format_password(password=result))
+    print(format_password(result=result))
     copy_clip(result)
     return result
 
 
-def _save_password(name_file: str, login: str, password: str):
+def _save_password(filename: str, username: str, user_password: str):
     """
-    :param login: Логин, которому принадлежит сгенерированый пароль [по умолчанию значение None]
-    :param password: Сгенерированый пароль [по умолчанию значение None]
-    :param name_file: Имя файла в котором будет сохранен сгенерированый пароль [имя файла по умолчанию key.txt]
+    :param username: Логин, которому принадлежит сгенерированый пароль [по умолчанию значение None]
+    :param user_password: Сгенерированый пароль [по умолчанию значение None]
+    :param filename: Имя файла в котором будет сохранен сгенерированый пароль [имя файла по умолчанию key.txt]
     :return:
     """
-    with open("{}/Desktop/{}.txt".format(os.getenv('HOME'), name_file), "a") as f:
-        f.write("Логин: {}\nПароль: {}".format(login, password))
+    with open("{}/Desktop/{}.txt".format(os.getenv('HOME'), filename), "a") as f:
+        f.write("Логин: {}\nПароль: {}".format(username, user_password))
 
 
-def run():
+if __name__ == '__main__':
     password = _random_pass()
     save = str(input("Сохранить его в файл? [y/N]: "))
     if save in yes:
@@ -63,10 +63,6 @@ def run():
         if name_file and login != '':
             _save_password(name_file, login, password)
         else:
-            _save_password(name_file='key', login='None', password=password)
+            _save_password(filename='key', username='None', user_password=password)
     else:
         print("Пароль не был сохранён, но мы не рекомендуем так делать!")
-
-
-if __name__ == '__main__':
-    run()
